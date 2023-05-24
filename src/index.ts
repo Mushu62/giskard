676113e81fb605aa9677e9037983a8a4f54267a8
Mushu62 <37047954+Mushu62@users.noreply.github.com>
2023-05-24 13:36:32 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import { greetUser } from '$utils/greet';
+// Obtenez l'élément de switcher
+const switcher = document.getElementById('switcher');
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+// Appliquez le mode sombre si l'utilisateur a choisi ce mode précédemment
+if (localStorage.getItem('darkMode') === 'true') {
+  document.body.classList.add('dark-mode');
+  switcher.classList.add('is-active');
+}
+
+// Ajoutez un écouteur d'événement pour le clic sur le switcher
+switcher.addEventListener('click', function () {
+  this.classList.toggle('is-active');
+  document.body.classList.toggle('dark-mode');
+
+  // Enregistrez la préférence de l'utilisateur dans le stockage local
+  if (this.classList.contains('is-active')) {
+    localStorage.setItem('darkMode', 'true');
+  } else {
+    localStorage.setItem('darkMode', 'false');
+  }
 });
